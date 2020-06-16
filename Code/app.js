53435bff81138562e1ce937c6f16713937ade78a
devjosegomez <devjosegomez@gmail.com>
2020-06-16 12:50:31 -0500

    Adding node modules

--- Code/app.js
+++ Code/app.js
@@ -1,4 +1,10 @@
+const validator = require ( 'validator' )
 const getNotes = require ( './notes.js' )
 
 let message = getNotes ()
 console.log ( message )
+
+console.log ( validator.isEmail ( 'devjosegomez@github.com' ) )
+console.log ( validator.isDate ( '2020-06-16' ) )
+console.log ( validator.isIBAN ( 'AT483200000012345864/' ) )
+console.log ( validator.isIBAN ( 'AT483200000012345864' ) )
